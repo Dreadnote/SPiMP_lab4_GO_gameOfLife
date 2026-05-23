b5fc8d9cd4e8f3c24fc9473c775a0b6f185e521f
Dreadnote <Dreadnote1337@gmail.com>
2026-05-23 17:56:35 +0300

    Итоговая версия с .exe

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -11,34 +12,34 @@ const (
 	screenWidth  = 1200
 	screenHeight = 800
 	gridSize     = 20
-	cellsX       = screenWidth / gridSize  // 60
-	cellsY       = screenHeight / gridSize // 40
+	uiHeight     = 100
+	gameHeight   = screenHeight - uiHeight
+	cellsX       = screenWidth / gridSize
+	cellsY       = gameHeight / gridSize
 )
 
-// Игровое состояние
 var (
 	grid             [cellsY][cellsX]bool
 	nextGrid         [cellsY][cellsX]bool
 	paused           = true
-	speedLevel       = 0 // 0 = 1x, 1 = 2x, 2 = 4x
-	speedMultipliers = []float64{1.0, 2.0, 4.0}
+	speedLevel       = 0
+	speedMultipliers = []float64{1, 2, 4, 8, 16, 32, 64}
 	lastUpdate       time.Time
 )
 
 func main() {
-	rl.InitWindow(screenWidth, screenHeight, "Conway's Game of Life - Лабораторная работа")
+	rl.InitWindow(screenWidth, screenHeight, "Conway's Game of Life - Lab")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
-	randomizeGrid(0.2) // Заполняем 20% клеток живыми
-
+	randomizeGrid(0.2)
 	lastUpdate = time.Now()
 
 	for !rl.WindowShouldClose() {
 		handleInput()
 
 		currentSpeed := speedMultipliers[speedLevel]
-		updateInterval := time.Duration(float64(1000/3)/currentSpeed) * time.Millisecond // 3 FPS обновления симуляции
+		updateInterval := time.Duration(float64(1000/6)/currentSpeed) * time.Millisecond
 
 		if !paused && time.Since(lastUpdate) >= updateInterval {
 			updateGame()
@@ -53,42 +54,50 @@ func main() {
 	}
 }
 
+// Обработка клавиатуры и мыши
 func handleInput() {
-	// Управление симуляцией
 	if rl.IsKeyPressed(rl.KeySpace) {
 		paused = !paused
 	}
-
 	if rl.IsKeyPressed(rl.KeyR) {
 		randomizeGrid(0.2)
 	}
-
 	if rl.IsKeyPressed(rl.KeyC) {
 		clearGrid()
 	}
-
-	// Изменение скорости: 1, 2, 3 на основной клавиатуре или NumPad
-	if rl.IsKeyPressed(rl.KeyOne) || rl.IsKeyPressed(rl.KeyKp1) {
-		speedLevel = 0
-	}
-	if rl.IsKeyPressed(rl.KeyTwo) || rl.IsKeyPressed(rl.KeyKp2) {
-		speedLevel = 1
-	}
-	if rl.IsKeyPressed(rl.KeyFour) || rl.IsKeyPressed(rl.KeyKp4) {
-		speedLevel = 2
-	}
-
-	// Выход
 	if rl.IsKeyPressed(rl.KeyEscape) {
 		rl.CloseWindow()
 	}
 
-	// Взаимодействие с мышью
+	// Выбор скорости цифрами 1-7
+	if rl.IsKeyPressed(rl.KeyOne) {
+		speedLevel = 0
+	}
+	if rl.IsKeyPressed(rl.KeyTwo) {
+		speedLevel = 1
+	}
+	if rl.IsKeyPressed(rl.KeyThree) {
+		speedLevel = 2
+	}
+	if rl.IsKeyPressed(rl.KeyFour) {
+		speedLevel = 3
+	}
+	if rl.IsKeyPressed(rl.KeyFive) {
+		speedLevel = 4
+	}
+	if rl.IsKeyPressed(rl.KeySix) {
+		speedLevel = 5
+	}
+	if rl.IsKeyPressed(rl.KeySeven) {
+		speedLevel = 6
+	}
+
+	// Взаимодействие с сеткой через мышь
 	mousePos := rl.GetMousePosition()
 	cellX := int(mousePos.X) / gridSize
 	cellY := int(mousePos.Y) / gridSize
 
-	if cellX >= 0 && cellX < cellsX && cellY >= 0 && cellY < cellsY {
+	if cellY >= 0 && cellY < cellsY && cellX >= 0 && cellX < cellsX {
 		if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
 			grid[cellY][cellX] = true
 		}
@@ -98,16 +107,16 @@ func handleInput() {
 	}
 }
 
+// Расчет следующего поколения клеток
 func updateGame() {
-	// Расчёт следующего поколения
 	for y := 0; y < cellsY; y++ {
 		for x := 0; x < cellsX; x++ {
 			neighbors := countNeighbors(x, y)
-			isAlive := grid[y][x]
+			alive := grid[y][x]
 
-			if isAlive && (neighbors == 2 || neighbors == 3) {
+			if alive && (neighbors == 2 || neighbors == 3) {
 				nextGrid[y][x] = true
-			} else if !isAlive && neighbors == 3 {
+			} else if !alive && neighbors == 3 {
 				nextGrid[y][x] = true
 			} else {
 				nextGrid[y][x] = false
@@ -115,7 +124,6 @@ func updateGame() {
 		}
 	}
 
-	// Копируем следующее поколение в текущее
 	for y := 0; y < cellsY; y++ {
 		for x := 0; x < cellsX; x++ {
 			grid[y][x] = nextGrid[y][x]
@@ -123,6 +131,7 @@ func updateGame() {
 	}
 }
 
+// Подсчет живых соседей (границы считаются мертвыми)
 func countNeighbors(x, y int) int {
 	count := 0
 	for dy := -1; dy <= 1; dy++ {
@@ -131,25 +140,17 @@ func countNeighbors(x, y int) int {
 				continue
 			}
 			nx, ny := x+dx, y+dy
-			// Тороидальные границы (мир закольцован)
-			if nx < 0 {
-				nx = cellsX - 1
-			} else if nx >= cellsX {
-				nx = 0
-			}
-			if ny < 0 {
-				ny = cellsY - 1
-			} else if ny >= cellsY {
-				ny = 0
-			}
-			if grid[ny][nx] {
-				count++
+			if nx >= 0 && nx < cellsX && ny >= 0 && ny < cellsY {
+				if grid[ny][nx] {
+					count++
+				}
 			}
 		}
 	}
 	return count
 }
 
+// Отрисовка игрового поля
 func drawGrid() {
 	for y := 0; y < cellsY; y++ {
 		for x := 0; x < cellsX; x++ {
@@ -168,40 +169,59 @@ func drawGrid() {
 			}
 		}
 	}
+	rl.DrawLine(0, gameHeight, screenWidth, gameHeight, rl.Red)
 }
 
+// Отрисовка нижней панели управления
 func drawUI() {
-	// Полупрозрачная панель внизу
-	rl.DrawRectangle(0, screenHeight-120, screenWidth, 120, rl.Fade(rl.Black, 0.85))
+	rl.DrawRectangle(0, gameHeight, screenWidth, uiHeight, rl.NewColor(45, 45, 45, 255))
+	rl.DrawRectangleLines(0, gameHeight, screenWidth, uiHeight, rl.Gray)
 
-	// Информация о состоянии
+	liveCount := 0
+	for y := 0; y < cellsY; y++ {
+		for x := 0; x < cellsX; x++ {
+			if grid[y][x] {
+				liveCount++
+			}
+		}
+	}
+
+	// ===== КОЛОНКА 1: Статус и скорость =====
 	statusText := "PAUSED"
 	statusColor := rl.Red
 	if !paused {
 		statusText = "RUNNING"
 		statusColor = rl.Green
 	}
+	rl.DrawText(statusText, 20, gameHeight+15, 22, statusColor)
 
-	speedText := "SPEED: 1x"
-	if speedLevel == 1 {
-		speedText = "SPEED: 2x"
-	} else if speedLevel == 2 {
-		speedText = "SPEED: 4x"
-	}
+	speedText := fmt.Sprintf("SPEED: %.0fx", speedMultipliers[speedLevel])
+	rl.DrawText(speedText, 20, gameHeight+50, 22, rl.White)
 
-	rl.DrawText(statusText, 20, screenHeight-90, 30, statusColor)
-	rl.DrawText(speedText, 20, screenHeight-55, 30, rl.White)
+	// ===== КОЛОНКА 2: Счетчик клеток =====
+	rl.DrawText("CELLS:", 220, gameHeight+15, 18, rl.LightGray)
+	liveText := fmt.Sprintf("%d / %d", liveCount, cellsX*cellsY)
+	rl.DrawText(liveText, 220, gameHeight+45, 28, rl.Yellow)
 
-	// Подсказки по управлению
-	rl.DrawText("[SPACE] Play/Pause", screenWidth-350, screenHeight-90, 20, rl.LightGray)
-	rl.DrawText("[1/2/4] Speed: 1x/2x/4x", screenWidth-350, screenHeight-65, 20, rl.LightGray)
-	rl.DrawText("[R] Randomize", screenWidth-350, screenHeight-40, 20, rl.LightGray)
-	rl.DrawText("[C] Clear", screenWidth-350, screenHeight-15, 20, rl.LightGray)
+	// ===== КОЛОНКА 3: Управление скоростью =====
+	rl.DrawText("SPEED (1-7):", 420, gameHeight+15, 18, rl.LightGray)
+	rl.DrawText("1=1x  2=2x  3=4x", 420, gameHeight+40, 18, rl.White)
+	rl.DrawText("4=8x  5=16x 6=32x", 420, gameHeight+60, 18, rl.White)
+	rl.DrawText("7=64x", 420, gameHeight+80, 18, rl.White)
 
-	// Подсказки по мыши
-	rl.DrawText("LMB: Create cell | RMB: Delete cell", 20, screenHeight-20, 20, rl.LightGray)
+	// ===== КОЛОНКА 4: Действия =====
+	rl.DrawText("CONTROLS:", 680, gameHeight+15, 18, rl.LightGray)
+	rl.DrawText("[SPACE] Play/Pause", 680, gameHeight+40, 18, rl.White)
+	rl.DrawText("[R] Randomize", 680, gameHeight+60, 18, rl.White)
+	rl.DrawText("[C] Clear", 680, gameHeight+80, 18, rl.White)
+
+	// ===== КОЛОНКА 5: Управление мышью =====
+	rl.DrawText("MOUSE:", 900, gameHeight+15, 18, rl.LightGray)
+	rl.DrawText("LMB: Create", 900, gameHeight+40, 18, rl.White)
+	rl.DrawText("RMB: Delete", 900, gameHeight+60, 18, rl.White)
 }
 
+// Случайное заполнение поля
 func randomizeGrid(density float32) {
 	for y := 0; y < cellsY; y++ {
 		for x := 0; x < cellsX; x++ {
@@ -210,6 +230,7 @@ func randomizeGrid(density float32) {
 	}
 }
 
+// Очистка поля
 func clearGrid() {
 	for y := 0; y < cellsY; y++ {
 		for x := 0; x < cellsX; x++ {
